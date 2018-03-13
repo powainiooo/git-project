5af7f31e2a11bd83e55efc12b2929e80f38f3b7b
powainiooo <powainiooo@163.com>
2018-03-13 16:04:59 +0800

    Add files via upload

--- tua-mobile/src/components/FooterNav.vue
+++ tua-mobile/src/components/FooterNav.vue
@@ -1,10 +1,11 @@
 <style>
-
+    .bottom-nav{ width: 100%; height: 100px; background-color: #171918; position: fixed; bottom: 0; left: 0;  z-index: 1000; box-shadow: 0 -10px 20px rgba(0,0,0,0.2); -webkit-box-shadow: 0 -10px 20px rgba(0,0,0,0.2); -moz-box-shadow: 0 -10px 20px rgba(0,0,0,0.2);}
+    .bottom-nav img{ width: 270px; margin: 25px 0 0 40px;}
 </style>
 
 <template>
-    <div>
-
+    <div class="bottom-nav">
+        <img src="@/assets/images/logo2.png">
     </div>
 </template>
 

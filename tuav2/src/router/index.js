d03801384621cfc8c4e2d09e9a8ec5957980c2bd
gogotsj <powainiooo@163.com>
2018-07-01 21:02:04 +0800

    s

--- tuav2/src/router/index.js
+++ tuav2/src/router/index.js
@@ -1,15 +1,39 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import index from '@/views/index'
+import about from '@/views/about'
+import works from '@/views/works'
+import workdetail from '@/views/workdetail'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+
+let router = new Router({
+    routes: [
+        {
+            path: '/',
+            redirect:'/index'
+        },
+        {
+            path:'/index',
+            name: 'index',
+            component: index
+        },
+        {
+            path:'/about',
+            name: 'about',
+            component: about
+        },
+        {
+            path:'/works',
+            name: 'works',
+            component: works
+        },
+        {
+            path:'/works/detail/:id',
+            name: 'workdetail',
+            component: workdetail
+        }
+    ]
+});
+export default router

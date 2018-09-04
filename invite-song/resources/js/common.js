1fed3829863d2e254aa360541bd9eba6a1350e98
powainiooo <zhang.jian58@byd.com>
2018-09-04 10:39:58 +0800

    's'

--- invite-song/resources/js/common.js
+++ invite-song/resources/js/common.js
@@ -12,14 +12,3 @@
     };
     recalc();
 })(document, window);
-var wwidth;
-var wheight;
-function setsize(){
-    wwidth = parseInt($(window).width());
-    wheight = parseInt($(window).height());
-    $(".wheight").css("height",wheight);
-}
-window.onload=function(){
-    setsize();
-    setTimeout(setsize,500);
-}

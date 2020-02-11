d008a9a05e26cc2ab5bab0dfe0be363392befdbe
gogotsj <powainiooo@163.com>
2020-02-11 22:23:41 +0800

    s

--- education/wepy.config.js
+++ education/wepy.config.js
@@ -2,35 +2,35 @@ const path = require('path')
 var prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  wpyExt: '.wpy',
-  eslint: true,
-  cliLogs: !prod,
-  build: {
-  },
-  resolve: {
-    alias: {
-      counter: path.join(__dirname, 'src/components/counter'),
-      '@': path.join(__dirname, 'src')
-    },
-    aliasFields: ['wepy', 'weapp'],
-    modules: ['node_modules']
-  },
-  compilers: {
-    less: {
-      compress: prod
-    },
-    babel: {
-      sourceMap: true,
-      presets: [
-        '@babel/preset-env'
-      ],
-      plugins: [
-        '@wepy/babel-plugin-import-regenerator'
-      ]
-    }
-  },
-  plugins: [],
-  appConfig: {
-    noPromiseAPI: ['createSelectorQuery']
-  }
+   wpyExt: '.wpy',
+   eslint: true,
+   cliLogs: !prod,
+   build: {
+   },
+   resolve: {
+      alias: {
+         counter: path.join(__dirname, 'src/components/counter'),
+         '@': path.join(__dirname, 'src')
+      },
+      aliasFields: ['wepy', 'weapp'],
+      modules: ['node_modules']
+   },
+   compilers: {
+      less: {
+         compress: prod
+      },
+      babel: {
+         sourceMap: true,
+         presets: [
+            '@babel/preset-env'
+         ],
+         plugins: [
+            '@wepy/babel-plugin-import-regenerator'
+         ]
+      }
+   },
+   plugins: [],
+   appConfig: {
+      noPromiseAPI: ['createSelectorQuery']
+   }
 }

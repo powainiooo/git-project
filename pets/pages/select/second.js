5fb3d0992da5e2b6707904d1d2c379e61aac28d0
ding.feng2 <ding.feng2@byd.com>
2020-03-18 17:46:15 +0800

    s

--- pets/pages/select/second.js
+++ pets/pages/select/second.js
@@ -1,4 +1,5 @@
 // pages/select/second.js
+import regeneratorRuntime from '../../utils/runtime.js'
 Page({
 
    /**
@@ -18,7 +19,10 @@ Page({
 	   	P: [{}, {}, {}, {}],
 	   	Y: [{}, {}, {}, {}],
 	   	Z: [{}, {}, {}, {}],
-	   }
+	   },
+	   scrollId: '',
+	   heights: {},
+	   screenHeight: 0
    },
 
    /**
@@ -29,21 +33,57 @@ Page({
 	   this.setData({
 		   type
 	   })
+	   const self = this
+	   wx.getSystemInfo({
+		   success(res) {
+			   self.data.screenHeight = res.screenHeight
+			   console.log(res.screenHeight)
+		   }
+	   })
+	   this.initSize()
    },
+	async initSize () {
+   	for (let i in this.data.dogList) {
+   		this.data.heights[i] = 0
+	   }
+   	for (let i in this.data.dogList) {
+		   const res = await this.getHeights(i)
+		   this.data.heights[i] = res.height
+	   }
+   	let add = 0
+   	let index = 0
+   	for (let i in this.data.heights) {
+   		if (index === 0) {
+			   this.data.heights[i] = 0
+		   } else {
+			   this.data.heights[i] += add
+		   }
+		   add = this.data.heights[i]
+		   index += 1
+	   }
+		console.log(this.data.heights)
+	},
+	getHeights (id) {
+   	return new Promise((resolve, reject) => {
+		   wx.createSelectorQuery().select(`#${id}`).boundingClientRect((rect) => {
+			   resolve(rect)
+		   }).exec()
+	   })
+	},
 	gotoBlock (e) {
    	const id = e.target.dataset.id
-		const query = wx.createSelectorQuery()
-		query.select(`#${id}`).boundingClientRect()
-		query.selectViewport().scrollOffset()
-		query.exec(res => {
-			console.log(res)
-			if (res[0] && res[1]) {
-				wx.pageScrollTo({
-					scrollTop: res[0].top + res[1].scrollTop - 150,
-					duration: 300
-				})
-			}
+		console.log(id)
+		this.setData({
+			scrollId: `${id}`
 		})
+	},
+	scroll (e) {
+   	// console.log(e.detail.scrollTop)
+	},
+	doNext () {
+   	wx.navigateTo({
+		   url: '/pages/package/package'
+	   })
 	},
    /**
     * 生命周期函数--监听页面初次渲染完成

6d617cc98af666dc35729ba946da05dc42d2d670
gogotsj <powainiooo@163.com>
2021-03-23 22:03:31 +0800

    s

---
+++ xiaofuH5-v2/js/common.js
@@ -0,0 +1,14 @@
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        isIOS = navigator.userAgent.match(/iphone|ipod|ipad/gi),
+        dpr = isIOS? Math.min(win.devicePixelRatio, 3) : 1,
+        dpr = window.top === window.self? dpr : 1; //被iframe引用时，禁止缩放
+    var recalc = function () {
+        var width = docEl.clientWidth;
+        if (width / dpr > 750) {
+            width = 750 * dpr;
+        }
+        docEl.style.fontSize = 100 * (width / 750) + 'px';
+    };
+    recalc();
+})(document, window);

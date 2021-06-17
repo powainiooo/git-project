2355cb66329adc9046334e8d14e389ffb9b14e65
gogotsj <powainiooo@163.com>
2021-06-17 23:22:57 +0800

    's'

---
+++ epinzu-app/src/pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    test index
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  },
+
+  components: {
+  },
+
+  methods: {
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+

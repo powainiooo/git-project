68e67ad1fb74dd2719ccf2f007f7dd868ea3821d
gogotsj <powainiooo@163.com>
2021-07-09 22:07:40 +0800

    s

--- xf-platform-v3/vue.config.js
+++ xf-platform-v3/vue.config.js
@@ -28,7 +28,7 @@ module.exports = {
   productionSourceMap: false,
   // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
   devServer: {
-    proxy: 'http://leesticket_dev.waiting8.com'
+    proxy: 'http://web.uat.leesticket.com'
     // proxy: 'https://editor.leesticket.com'
   }
 }
